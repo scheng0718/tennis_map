89a5e2836f7dfca38196f809032dd59d253213f5
scheng0718 <scheng0718@gmail.com>
2023-12-07 17:49:31 -0800

    feat: create middleware for authentication and admin's authentication

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,23 @@
 const express = require('express')
 const router = express.Router()
-const { apiErrorHandler } = require('../middlewares/error-handler')
 const courtController = require('../controllers/court-controller')
 const commentController = require('../controllers/comment-controller')
 const navigationController = require('../controllers/navigation-controller')
 const admin = require('./modules/admin')
+const { authenticated, authenticatedAdmin } = require('../middlewares/auth')
+const { apiErrorHandler } = require('../middlewares/error-handler')
 
-router.use('/admin', admin)
+router.use('/admin', authenticated, authenticatedAdmin, admin)
 
-router.get('/courts/search', courtController.getCourtsBySearch)
-router.get('/courts/nearby', courtController.getNearByCourts)
-router.get('/courts/map', courtController.getCourtsByMap)
-router.get('/courts/:courtId', courtController.getCourt)
-router.get('/courts', courtController.getCourts)
+router.get('/courts/search', authenticated, courtController.getCourtsBySearch)
+router.get('/courts/nearby', authenticated, courtController.getNearByCourts)
+router.get('/courts/map', authenticated, courtController.getCourtsByMap)
+router.get('/courts/:courtId', authenticated, courtController.getCourt)
+router.get('/courts', authenticated, courtController.getCourts)
 
-router.get('/comments/:courtId', commentController.getCommentsByCourt)
+router.get('/comments/:courtId', authenticated, commentController.getCommentsByCourt)
 
-router.get('/navigation/:courtId', navigationController.getDirection)
+router.get('/navigation/:courtId', authenticated, navigationController.getDirection)
 
 router.use('/', apiErrorHandler)
 
