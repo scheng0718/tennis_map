3e8e29d99452783a9fadb25a7851e909a0e093d7
scheng0718 <scheng0718@gmail.com>
2026-04-20 21:45:51 -0700

    feature: add three methods in controller

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -2,6 +2,15 @@ const commentServices = require('../services/comment-services')
 const commentController = {
   getCommentsByCourt: (req, res, next) => {
     commentServices.getCommentsByCourt(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  },
+  createComment: (req, res, next) => {
+    commentServices.createComment(req, (err, data) => err ? next(err) : res.status(201).json({ status: 'success', data }))
+  },
+  updateComment: (req, res, next) => {
+    commentServices.updateComment(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  },
+  deleteComment: (req, res, next) => {
+    commentServices.deleteComment(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   }
 }
 module.exports = commentController
