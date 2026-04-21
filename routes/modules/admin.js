e8b1c74b0f904c0d769e14cd4c3cb8e2d8628a61
scheng0718 <scheng0718@gmail.com>
2026-04-20 21:46:34 -0700

    feature: implement admin route

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,4 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const adminController = require('../../controllers/admin-controller')
+
+router.get('/courts', adminController.getCourts)
+router.post('/courts', adminController.createCourt)
+router.put('/courts/:courtId', adminController.updateCourt)
+router.delete('/courts/:courtId', adminController.deleteCourt)
+
+router.get('/users', adminController.getUsers)
+router.delete('/users/:userId', adminController.deleteUser)
+
+router.get('/comments', adminController.getComments)
+router.delete('/comments/:commentId', adminController.deleteComment)
 
 module.exports = router
